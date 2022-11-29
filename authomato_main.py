c6c4ea6d8f8a89b03a4eb8c68d5290c873e2526a
p_kuzichkin <kuzichkinpa@gmail.com>
2022-11-29 10:17:07 +0400

    fix

--- authomato_main.py
+++ authomato_main.py
@@ -3,26 +3,26 @@ from authomato.nfa import NFA
 
 
 def main():
-    # d = DFA(is_file=True, path='dfa.txt')
-    # if d.accept('aaabcc'):
-    #     print('Цепочка распознана')
-    # else:
-    #     print('Цепочка не распознана')
+    d = DFA(is_file=True, path='dfa.txt')
+    if d.accept('aaabc'):
+        print('Цепочка распознана')
+    else:
+        print('Цепочка не распознана')
+
+    d.show_data()
+
+    # n = NFA(is_file=True, path='nfa.txt')
+    # n.show_data()
     #
-    # d.show_data()
-
-    n = NFA(is_file=True, path='nfa.txt')
-    n.show_data()
-
-    print('-' * 30)
-
-    n = n.removeEpsilonTransitions()
-    n.show_data()
-
-    print('-' * 30)
-
-    n = n.getDFA()
-    n.show_data()
+    # print('-' * 30)
+    #
+    # n = n.removeEpsilonTransitions()
+    # n.show_data()
+    #
+    # print('-' * 30)
+    #
+    # n = n.getDFA()
+    # n.show_data()
     # if n.accept('bbaa'):
     #     print('Цепочка распознана')
     # else:
